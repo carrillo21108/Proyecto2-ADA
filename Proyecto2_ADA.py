8fd1fc7196acd707ff7f03d4d77d622aa3a6cda4
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2024-04-02 18:56:36 -0600

    Implementación cantidad de pasos

--- Proyecto2_ADA.py
+++ Proyecto2_ADA.py
@@ -1,6 +1,12 @@
+# Numero de pasos realizados
+cant_pasos = 0
+
 # Enfoque DaC
-def lcs_dac(A, B, i, j): 
-    if i == 0 or j == 0: 
+def lcs_dac(A, B, i, j):
+    global cant_pasos
+    cant_pasos+=1
+    
+    if i == 0 or j == 0:
         return 0
     elif A[i-1] == B[j-1]: 
         return 1 + lcs_dac(A, B, i-1, j-1) 
@@ -9,9 +15,12 @@ def lcs_dac(A, B, i, j):
   
 # Enfoque Programacion Dinamica    
 def lcs_pd(A, B, i, j, dp): 
+    global cant_pasos
+    cant_pasos+=1
+    
     if (i == 0 or j == 0): 
         return 0
-    elif (dp[i][j] != -1): 
+    elif (dp[i][j] != -1):
         return dp[i][j] 
     elif A[i-1] == B[j-1]: 
         dp[i][j] = 1 + lcs_pd(A, B, i-1, j-1, dp) 
@@ -19,14 +28,19 @@ def lcs_pd(A, B, i, j, dp):
     else:
         dp[i][j] = max(lcs_pd(A, B, i-1, j, dp), lcs_pd(A, B, i, j-1, dp)) 
         return dp[i][j] 
-# Main
-if __name__ == '__main__': 
-    A = "AGGTAB"
-    B = "GXTXAYB"
+
+
+A = "AGGTAB"
+B = "GXTXAYB"
   
-    m = len(A) 
-    n = len(B) 
-    dp = [[-1 for i in range(n+1)]for j in range(m+1)] 
+m = len(A) 
+n = len(B) 
+dp = [[-1 for i in range(n+1)]for j in range(m+1)] 
   
-    print("Longitud de LCS con enfoque DaC: ", lcs_dac(A, B, m, n))
-    print("Longitud de LCS con enfoque PD: ", lcs_pd(A, B, m, n, dp))
+print("Longitud de LCS con enfoque DaC: ", lcs_dac(A, B, m, n))
+print("Tiempo de ejecucion: ", cant_pasos)
+input("Presione [Enter] para continuar\n")
+
+cant_pasos=0
+print("Longitud de LCS con enfoque PD: ", lcs_pd(A, B, m, n, dp))
+print("Tiempo de ejecucion: ", cant_pasos)
